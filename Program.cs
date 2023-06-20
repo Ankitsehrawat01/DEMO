9a270560fdb1372ea4a6bc3755abe54952d6293f
Ankitsehrawat01 <sehrawata328@gmail.com>
2023-06-20 15:53:09 +0530

    [Ankirt Kumar]: Added Employee is present or absent

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            //WageComputation.EmpWage();
+
+            //Console.WriteLine("Enter a Number");
+            //string num = Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ WageComputation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    public class WageComputation
+    {
+        public static void EmpWage()
+        {
+            int Is_Present = 1;
+            Random randomobj = new Random();
+            int empCheck = randomobj.Next(0, 2);
+            if (empCheck == Is_Present)
+            {
+                Console.WriteLine("Emloyee is Present");
+                
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                
+            }
+        }
+    }
+}
